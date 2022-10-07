2cf24d9eac71eb29da375877a9351a32a1256389
Oriol Tintó <oriol.tinto@lmu.de>
2022-10-07 14:58:48 +0200

    Use quantile .99 instead of max to compute the probabilities.

--- app/sources/data_process.py
+++ app/sources/data_process.py
@@ -14,17 +14,19 @@ def convert_to_probabilities(data_array: xarray.DataArray, variable: Variables) 
     :return data_array:
     """
     min_value = data_array.min().values
-    max_value = data_array.max().values
+
+    max_value = np.nanquantile(data_array.values, .99)
     number_of_steps = 100
     steps = np.linspace(min_value, max_value, number_of_steps)
     times = data_array["time"]
     members = data_array.member
     members = [idx for idx, m in enumerate(members) if m.values != "Main"]
     data_array = data_array.isel(member=members)
-    number_of_members = data_array["member"].size
+
     prob_array = np.zeros((len(times), len(steps)))
 
     for t_idx, t in enumerate(times):
+        number_of_members = np.count_nonzero(~np.isnan(data_array.sel(time=t).values))
         for s_idx, s in enumerate(steps):
             prob_array[t_idx, s_idx] = (data_array.sel(time=t) > s).sum() / number_of_members * 100.
 
